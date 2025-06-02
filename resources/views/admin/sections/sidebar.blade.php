0389e231faea3bc693efaeb4c8707fa663573a70
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-06-02 16:30:35 +0430

    add the image and teacher sidebar

--- resources/views/admin/sections/sidebar.blade.php
+++ resources/views/admin/sections/sidebar.blade.php
@@ -47,12 +47,30 @@
                     <ul class="sub-menu" aria-expanded="false">
                         <li><a href="{{ route('add.student') }}">Add Student</a></li>
                         <li><a href="{{ route('manage.student') }}">Manage Student</a></li>
-                        <li><a href="{{ route('add.teacher') }}">Add Teacher</a></li>
+                        {{-- <li><a href="{{ route('add.teacher') }}">Add Teacher</a></li> --}}
 
-                        
+
 
                     </ul>
                 </li>
+
+
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="ri-account-circle-line"></i>
+                        <span>Teacher</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+
+                        <li><a href="{{ route('add.teacher') }}">Add Teacher</a></li>
+
+
+
+                    </ul>
+                </li>
+
+
 
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
